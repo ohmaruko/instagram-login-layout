adc945e00409db8a448faa81deefb21a4529e46c
madoka <mnogaki@my.bcit.ca>
2024-01-06 12:14:14 -0800

    updated Footer components

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,25 +1,16 @@
 import styles from './Footer.module.css'
 
 export default function Footer() {
+    const links = ["Meta", "About", "Blog", "Jobs", "Helps", "API", "Privacy", "Terms", "Locations", "Instagram Lite", "Threads", "Contact Uploading & Non-Users", "Meta Verified"];
     return (
         <div className={styles.footerContainer}>
             <footer className={styles.footer}>
-            <a href='#' className={styles.link}>Meta</a>
-            <a href='#' className={styles.link}>About</a>
-            <a href='#' className={styles.link}>Blog</a>
-            <a href='#' className={styles.link}>Jobs</a>
-            <a href='#' className={styles.link}>Helps</a>
-            <a href='#' className={styles.link}>API</a>
-            <a href='#' className={styles.link}>Privacy</a>
-            <a href='#' className={styles.link}>Terms</a>
-            <a href='#' className={styles.link}>Locations</a>
-            <a href='#' className={styles.link}>Instagram Lite</a>
-            <a href='#' className={styles.link}>Threads</a>
-            <a href='#' className={styles.link}>Contact Uploading & Non-Users</a>
-            <a href='#' className={styles.link}>Meta Verified</a>
-            <br />
-            &copy; 2024 Instagram from Meta
-        </footer>
+                <div className={styles.links}>
+                    {links.map((links) => <a href='#' className={styles.link}>{links}</a>)}
+                </div>
+                <br />
+                <p>&copy; 2024 Instagram from Meta</p>
+            </footer>
         </div>
     )
 }
